ad7840604ab67f097971fd45eba30e690e18a31d
sebas2210 <bastianjyr@gmail.com>
2022-06-14 22:57:11 -0500

    rama sebastian

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 // import './App.css';
 // import './Style.css';
 // test comentario
-
+// f
 import Slider from './components/Slider/Slider';
 import Services from './components/Services/Services';
 import Footer from './components/Footer/Footer';
