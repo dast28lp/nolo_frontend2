d5078f8d99110f03d74d9aa51ea31ef8bf3444d3
sebas2210 <bastianjyr@gmail.com>
2022-06-14 22:23:54 -0500

    test commit sebastian

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 // import './App.css';
 // import './Style.css';
+// test comentario
 
 import Slider from './components/Slider/Slider';
 import Services from './components/Services/Services';
